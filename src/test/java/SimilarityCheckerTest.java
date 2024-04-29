575079acc0993e093e4f9bc87f99bb65e72b433d
ghdwlsrl3229 <ghdwlsrl3229@naver.com>
2024-04-29 14:19:27 +0900

    문자열 길이가 2배 이상 차이가 난다면 0점 획득

--- src/test/java/SimilarityCheckerTest.java
+++ src/test/java/SimilarityCheckerTest.java
@@ -18,4 +18,26 @@ class SimilarityCheckerTest {
         assertEquals(60, sc.getLengthScore("ABCD", "DEDF"));
         assertEquals(60, sc.getLengthScore("ABCAG", "DGEAF"));
     }
+
+    @Test
+    void score0IfLengthDiffMoreThan2Times() {
+        SimilarityChecker sc = new SimilarityChecker();
+
+        assertEquals(0, sc.getLengthScore("A", "AB"));
+        assertEquals(0, sc.getLengthScore("A", "ABC"));
+        assertEquals(0, sc.getLengthScore("BA", "ABCD"));
+        assertEquals(0, sc.getLengthScore("BA", "ABCDEF"));
+        assertEquals(0, sc.getLengthScore("BAC", "ABCDEF"));
+    }
+
+    @Test
+    void score0IfLengthDiffMoreThan2Times2() {
+        SimilarityChecker sc = new SimilarityChecker();
+
+        assertEquals(0, sc.getLengthScore("AB", "A"));
+        assertEquals(0, sc.getLengthScore("ABC", "A"));
+        assertEquals(0, sc.getLengthScore("ABCD", "BA"));
+        assertEquals(0, sc.getLengthScore("ABCDEF", "BA"));
+        assertEquals(0, sc.getLengthScore("ABCDEF", "BAC"));
+    }
 }
