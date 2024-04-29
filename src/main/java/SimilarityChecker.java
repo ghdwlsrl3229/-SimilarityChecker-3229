c1f986e68b939561534df356afe69366e4bafd21
ghdwlsrl3229 <ghdwlsrl3229@naver.com>
2024-04-29 16:13:38 +0900

    두 문자열에 같은 종류의 알파벳이 사용되었다면 최대점수 40점을 획득하는 함수 구현

--- src/main/java/SimilarityChecker.java
+++ src/main/java/SimilarityChecker.java
@@ -1,2 +1,5 @@
 public class SimilarityChecker {
+    public int getAlphaScore(String str1, String str2) {
+        return 40;
+    }
 }
--- src/test/java/SimilarityCheckerTest.java
+++ src/test/java/SimilarityCheckerTest.java
@@ -1,5 +1,14 @@
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class SimilarityCheckerTest {
-
+    @Test
+    void SameAlphaTest() {
+        SimilarityChecker sc = new SimilarityChecker();
+        assertEquals(40, sc.getAlphaScore("ABCD", "ABCD"));
+        assertEquals(40, sc.getAlphaScore("ABCD", "DBACAA"));
+        assertEquals(40, sc.getAlphaScore("A", "AAA"));
+        assertEquals(40, sc.getAlphaScore("CBA", "ABC"));
+    }
 }
