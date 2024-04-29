ee886edfe4cb682c0d821866ac356f726f101fc3
ghdwlsrl3229 <ghdwlsrl3229@naver.com>
2024-04-29 14:59:15 +0900

    글자 수 검사 함수 리팩토링

--- src/main/java/SimilarityChecker.java
+++ src/main/java/SimilarityChecker.java
@@ -1,11 +1,25 @@
 public class SimilarityChecker {
     public int getLengthScore(String str1, String str2) {
-        if (str1.length() == str2.length()) return 60;
+        if (isSameLength(str1, str2)) return 60;
+        if (is2TimesOverLengthDiff(str1, str2)) return 0;
+        return getPartialScore(str1, str2);
+    }
+
+    private int getPartialScore(String str1, String str2) {
         int shortLength = getShorterStringLength(str1, str2);
         int longLength = getLongerStringLength(str1, str2);
-        if (shortLength * 2 < longLength) return 0;
         int gap = longLength - shortLength;
-        return 60 * (shortLength - gap) / shortLength;
+        return (int)((1 - 1.0 * gap / shortLength) * 60);
+    }
+
+    private boolean is2TimesOverLengthDiff(String str1, String str2) {
+        int shortLength = getShorterStringLength(str1, str2);
+        int longLength = getLongerStringLength(str1, str2);
+        return shortLength * 2 < longLength;
+    }
+
+    private boolean isSameLength(String str1, String str2) {
+        return str1.length() == str2.length();
     }
 
     public int getShorterStringLength(String str1, String str2) {
