4442282a8ddba0b43f4c27ac7c3406ea6d17645d
ghdwlsrl3229 <ghdwlsrl3229@naver.com>
2024-04-29 16:24:05 +0900

    문자열에서 A~Z 범위의 알파벳이 존재하는지 여부를 boolean 형태로 리턴하는 함수 구현

--- src/main/java/SimilarityChecker.java
+++ src/main/java/SimilarityChecker.java
@@ -1,5 +1,16 @@
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 public class SimilarityChecker {
     public int getAlphaScore(String str1, String str2) {
         return 40;
+    }
+
+    public boolean[] getAlphaExistList(String str) {
+        boolean[] result = new boolean[26];
+        for (char ch : str.toCharArray()) {
+            result[ch - 'A'] = true;
+        }
+        return result;
     }
 }
--- src/test/java/SimilarityCheckerTest.java
+++ src/test/java/SimilarityCheckerTest.java
@@ -1,5 +1,8 @@
 import org.junit.jupiter.api.Test;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class SimilarityCheckerTest {
@@ -10,5 +13,18 @@ class SimilarityCheckerTest {
         assertEquals(40, sc.getAlphaScore("ABCD", "DBACAA"));
         assertEquals(40, sc.getAlphaScore("A", "AAA"));
         assertEquals(40, sc.getAlphaScore("CBA", "ABC"));
+    }
+
+    @Test
+    void AllDifferentAlphaTest() {
+        SimilarityChecker sc = new SimilarityChecker();
+        boolean[] actual = sc.getAlphaExistList("AGEF");
+        boolean[] expected = new boolean[26];
+        expected['A' - 'A'] =  true;
+        expected['G' - 'A'] =  true;
+        expected['E' - 'A'] =  true;
+        expected['F' - 'A'] =  true;
+
+        assertArrayEquals(expected, actual);
     }
 }
