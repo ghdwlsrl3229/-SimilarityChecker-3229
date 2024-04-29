adb59b7e9cdbe1c69756de2b6420475ad76bd4e1
ghdwlsrl3229 <ghdwlsrl3229@naver.com>
2024-04-29 14:27:46 +0900

    더 짧은 문자열의 길이를 반환하는 함수 구현

--- src/main/java/SimilarityChecker.java
+++ src/main/java/SimilarityChecker.java
@@ -3,4 +3,9 @@ public class SimilarityChecker {
         if (str1.length() == str2.length()) return 60;
         return 0;
     }
+
+    public int getShorterStringLength(String str1, String str2) {
+        if (str1.length() < str2.length()) return str1.length();
+        return str2.length();
+    }
 }
--- src/test/java/SimilarityCheckerTest.java
+++ src/test/java/SimilarityCheckerTest.java
@@ -1,18 +1,23 @@
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class SimilarityCheckerTest {
+    SimilarityChecker sc;
+
+    @BeforeEach
+    void setUp() {
+        sc = new SimilarityChecker();
+    }
+
     @Test
     void createSimilarityChecker() {
-        SimilarityChecker sc = new SimilarityChecker();
         assertNotNull(sc);
     }
 
     @Test
     void score60IfSameLength() {
-        SimilarityChecker sc = new SimilarityChecker();
-
         assertEquals(60, sc.getLengthScore("A", "D"));
         assertEquals(60, sc.getLengthScore("AB", "DE"));
         assertEquals(60, sc.getLengthScore("ABCD", "DEDF"));
@@ -21,8 +26,6 @@ class SimilarityCheckerTest {
 
     @Test
     void score0IfLengthDiffMoreThan2Times() {
-        SimilarityChecker sc = new SimilarityChecker();
-
         assertEquals(0, sc.getLengthScore("A", "AB"));
         assertEquals(0, sc.getLengthScore("A", "ABC"));
         assertEquals(0, sc.getLengthScore("BA", "ABCD"));
@@ -32,12 +35,19 @@ class SimilarityCheckerTest {
 
     @Test
     void score0IfLengthDiffMoreThan2Times2() {
-        SimilarityChecker sc = new SimilarityChecker();
-
         assertEquals(0, sc.getLengthScore("AB", "A"));
         assertEquals(0, sc.getLengthScore("ABC", "A"));
         assertEquals(0, sc.getLengthScore("ABCD", "BA"));
         assertEquals(0, sc.getLengthScore("ABCDEF", "BA"));
         assertEquals(0, sc.getLengthScore("ABCDEF", "BAC"));
+    }
+
+    @Test
+    void getShorterString() {
+        assertEquals(3, sc.getShorterStringLength("ABCDE", "ABC"));
+        assertEquals(1, sc.getShorterStringLength("A", "ABC"));
+        assertEquals(2, sc.getShorterStringLength("ABCDE", "BC"));
+        assertEquals(3, sc.getShorterStringLength("ABCDEW", "ABC"));
+        assertEquals(2, sc.getShorterStringLength("AC", "AB"));
     }
 }
