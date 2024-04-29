d31ad07119fa36380dfc70d6027a0ce3cdcf48b8
ghdwlsrl3229 <ghdwlsrl3229@naver.com>
2024-04-29 14:15:37 +0900

    같은 글자수일 때 60점 획득

--- src/main/java/SimilarityChecker.java
+++ src/main/java/SimilarityChecker.java
@@ -1,2 +1,6 @@
 public class SimilarityChecker {
+    public int getLengthScore(String str1, String str2) {
+        if (str1.length() == str2.length()) return 60;
+        return 0;
+    }
 }
--- src/test/java/SimilarityCheckerTest.java
+++ src/test/java/SimilarityCheckerTest.java
@@ -1,5 +1,21 @@
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class SimilarityCheckerTest {
+    @Test
+    void createSimilarityChecker() {
+        SimilarityChecker sc = new SimilarityChecker();
+        assertNotNull(sc);
+    }
 
+    @Test
+    void score60IfSameLength() {
+        SimilarityChecker sc = new SimilarityChecker();
+
+        assertEquals(60, sc.getLengthScore("A", "D"));
+        assertEquals(60, sc.getLengthScore("AB", "DE"));
+        assertEquals(60, sc.getLengthScore("ABCD", "DEDF"));
+        assertEquals(60, sc.getLengthScore("ABCAG", "DGEAF"));
+    }
 }
